6abec05dfb28a24a6bf111ff1c32ee524bf719fb
elxavi82 <ejavier19@gmail.com>
2020-11-13 11:29:03 -0500

    step 32

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,29 @@
 import './App.css';
 import React from 'react';
+import SearchResults from '../SearchResults/SearchResults';
 
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults = [{
+        name: 'Smells Like Teen Spirit',
+        artist: 'Nirvana',
+        album: 'Nevermind',
+        id: 75 
+      },
+    {
+      name: 'Come As You Are',
+      artist: 'Nirvana',
+      album: 'Nevermind',
+      id: 76
+    }]
+    }
+
+
+  }
+
   render() {
     return (
 <div>
@@ -9,7 +31,7 @@ class App extends React.Component{
   <div className="App">
     {/*<!-- Add a SearchBar component -->*/}
     <div className="App-playlist">
-      {/*<!-- Add a SearchResults component -->*/}
+      {<SearchResults searchResults={this.state.searchResults} />}
       {/*<!-- Add a Playlist component -->*/}
     </div>
   </div>
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component{
+    
+    renderAction(){
+        let isRemoval;
+        let buttonComponent;
+
+        if(isRemoval){
+            buttonComponent = <button className='Track-action'>-</button>;
+        }else {
+            buttonComponent = <button className='Track-action'>+</button>;
+        }
+
+        return buttonComponent;
+    }
+
+    render(){
+        return(
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{/*<!-- track name will go here -->*/}</h3>
+                    <p>{/*<!-- track artist will go here-->*/} | {/*<!-- track album will go here -->*/}</p>
+                </div>
+                <button className="Track-action">{/*<!-- + or - will go here -->*/}</button>
+            </div>
+        );
+    }
+}
+
+
+export default Track;
